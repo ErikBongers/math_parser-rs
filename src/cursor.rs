03f26f3654239226d74285a88c574eca7a01b9ff
Erik Bongers <mail@erikbongers.net>
2023-12-09 18:50:47 +0100

    Testing out Cursor from rustc_lexer

---
+++ src/cursor.rs
@@ -0,0 +1,29 @@
+use std::str::Chars;
+
+pub struct Cursor<'a> {
+    chars: Chars<'a>,
+}
+
+pub(crate) const EOF_CHAR: char = '\0';
+
+impl<'a> Cursor<'a> {
+    pub fn new (text: &str) -> Cursor {
+        Cursor {
+            chars: text.chars()
+        }
+    }
+
+    pub fn peek(&self) -> char {
+        self.chars.clone().next().unwrap_or(EOF_CHAR)
+    }
+
+    pub fn peek_second(&self) -> char {
+        let mut iter = self.chars.clone();
+        iter.next();
+        iter.next().unwrap_or(EOF_CHAR)
+    }
+
+    pub fn next(&mut self) -> char {
+        self.chars.next().unwrap_or(EOF_CHAR)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+mod cursor;
+
+use std::fs;
+use crate::cursor::Cursor;
+
 fn main() {
-    println!("Hello, world!");
+    let file_path = r"data/source1.txt";
+    let result = fs::read_to_string(file_path);
+    let Ok(txt) = result
+        else {
+            println!("File ni gevonne...");
+            return;
+        };
+    parse(&txt);
+}
+
+fn parse(txt : &str) {
+    println!("{0}", txt);
+    let mut cur = Cursor::new(txt);
+    let c = cur.peek();
+    println!("first: {0}", c);
+    let c = cur.peek_second();
+    println!("second: {0}", c);
+    let c = cur.peek(); //should be same
+    println!("first: {0}", c);
+    let c = cur.next(); // still same
+    println!("first: {0}", c);
+    let c = cur.peek(); // next char.
+    println!("first: {0}", c);
 }
