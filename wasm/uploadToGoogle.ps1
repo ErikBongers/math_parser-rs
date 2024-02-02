a2b0888c2e197e82ebca194193ec7df6a31460d3
Erik Bongers <mail@erikbongers.net>
2024-02-02 22:51:00 +0100

    Replace google upload command to do it all in one command.

--- wasm/uploadToGoogle.ps1
+++ wasm/uploadToGoogle.ps1
@@ -1,2 +1,1 @@
-gsutil rsync -r www gs://mathparser/
-gsutil setmeta -h "Cache-Control:private, max-age=0, no-transform" gs://mathparser/*.*
+gcloud storage rsync www gs://mathparser/ --recursive --cache-control="private, max-age=0, no-transform" --exclude='pack[/\\]package\.json$"|"pack[/\\]\.gitignore$'
